e4dc63cc96229e8d2797eb466d4807be816a014a
Savitha Venkatesh <savithav2000@gmail.com>
2024-02-19 02:57:54 -0600

    updated fishinghole code for fishing

--- src/edu/westga/cs6312/fishing/model/FishingHole.java
+++ src/edu/westga/cs6312/fishing/model/FishingHole.java
@@ -70,6 +70,28 @@ public class FishingHole {
 	}
 
 	/**
+	 * Simulates a fishing attempt in this fishing hole. The success of the attempt
+	 * depends on the presence and type of fish in the hole. If a fish type is
+	 * present, it calculates the number of fish caught based on the specific fish
+	 * type's catch logic. After a successful catch, the fish type in the hole is
+	 * set to null, simulating that all fish of that type have been caught and
+	 * removed from the hole.
+	 *
+	 * @return the number of fish caught during the fishing attempt. Returns 0 if no
+	 *         fish are present or if the catch was unsuccessful.
+	 */
+	public int fish() {
+		if (this.currentFishType == null) {
+			System.out.println("No fish available to catch at this location.");
+			return 0;
+		} else {
+			int fishCaught = this.currentFishType.catchFish();
+			this.currentFishType = null;
+			return fishCaught;
+		}
+	}
+
+	/**
 	 * Returns a string representation of this FishingHole.
 	 * 
 	 * @return A string representation of this FishingHole.
